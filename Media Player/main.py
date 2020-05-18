7e611533dfd6ee2a4d93816cc919b08d6ee2e0ad
shurawz <xuchepran9@gmail.com>
2020-05-18 19:33:52 +0545

    Horizontal Volume Scale has been added.

--- Media Player/main.py
+++ Media Player/main.py
@@ -14,6 +14,11 @@ def stop_btn():
     mixer.music.stop()
 
 
+def set_vol(val):
+    volume = int(val) / 100
+    mixer.music.set_volume(volume)
+
+
 window = Tk()  # Creates a window
 window.title("Music Player")
 window.iconbitmap(r'Photos\MPicon.ico')  # r stands for Random String
@@ -29,6 +34,10 @@ playButton.pack()
 photo1 = PhotoImage(file='Photos\stop.png')
 stopButton = Button(window, image=photo1, command=stop_btn)
 stopButton.pack()
+
+vol_scale = Scale(window, from_=0, to=100, orient=HORIZONTAL, command=set_vol)
+vol_scale.set(4)
+vol_scale.pack()
 
 window.mainloop()  # Displays the window
 
